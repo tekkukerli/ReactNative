cd1b87c2672ef775835b2d269a8c61d05460c5a4
tekkukerli <kerlitekku@gmail.com>
2021-03-12 21:49:49 +0200

    added rating stars

--- screens/ProductPage.js
+++ screens/ProductPage.js
@@ -3,15 +3,45 @@
 import React, { Component } from 'react';
 import {  StyleSheet, Button, View, Text, Image } from 'react-native';
 
+import StarRating from 'react-native-star-rating';
+
+// Import vector icons
+import Icon from 'react-native-vector-icons/FontAwesome';
+
 class ProductPage extends Component {
+
+   constructor(props) {
+      super(props);
+      this.state = {
+        starCount: 4
+      };
+    }
+
+    onStarRatingPress(rating) {
+      this.setState({
+        starCount: rating
+      });
+    }
+
   render() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <View style={{ flex: 1,  justifyContent: 'center' }}>
 
-        <Text>ProductPage Screen</Text>
-        <Image source={require('../Resources/pitsa.jpg')} style={styles.image}/>
+        <Image source={require('../Resources/pitsa.png')} style={styles.image}/>
+        <Text>Hot dogi pizza mozzarella ja cheddari juustuga!</Text>
+        <Text>Rannarootsi</Text>
+
+        <StarRating
+            disabled={false}
+            maxStars={5}
+            fullStarColor={'orange'}
+            emptyStarColor={'orange'}
+            rating={this.state.starCount}
+            selectedStar={(rating) => this.onStarRatingPress(rating)}
+         />
 
       </View>
+
     );
   }
 }
